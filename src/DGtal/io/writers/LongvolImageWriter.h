071ec1d344d31856ba1859b4d0f8be1adfc0956d
Jeremy Gaillard <jeremy.gaillard@insa-lyon.fr>
2012-07-03 10:32:57 +0200

    Added class LongvolImageWriter. Same as LongvolWriter except it doesn't use a colormap and exports directly the values in the image to a longvol file

---
+++ src/DGtal/io/writers/LongvolImageWriter.h
@@ -0,0 +1,122 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file LongvolImageWriter.h
+ * @author Jérémy Gaillard (\c jeremy.gaillard@insa-lyon.fr )
+ * Institut National des Sciences Appliquées - INSA, France
+ *
+ * @date 2012/07/03
+ *
+ * Header file for module LongvolImageWriter.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(LongvolImageWriter_RECURSES)
+#error Recursive header files inclusion detected in LongvolWriter.h
+#else // defined(LongvolImageWriter_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define LongvolImageWriter_RECURSES
+
+#if !defined LongvolImageWriter_h
+/** Prevents repeated inclusion of headers. */
+#define LongvolImageWriter_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include <string>
+#include "DGtal/base/Common.h"
+#include "DGtal/io/colormaps/CColorMap.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // template class LongvolWriter
+  /**
+   * Description of template struct 'LongvolImageWriter' <p>
+   * \brief Aim: Export a 3D Image using the Longvol formats
+   * (volumetric image with DGtal::uint64_t value type).
+   *
+   * The file format contains an ASCII header and a raw binary array
+   * (little-endian uint64t).
+   *
+   * @tparam TImage the Image type.
+   * @tparam TColormap the type of the colormap to use in the export.
+   *
+   * @see testLongvol.cpp
+   */
+  template <typename TImage>
+  struct LongvolImageWriter
+  {
+    // ----------------------- Standard services ------------------------------
+
+    BOOST_STATIC_ASSERT(TImage::Domain::dimension == 3);
+    
+    typedef TImage Image;
+    typedef typename TImage::Value Value;
+
+    /** 
+     * Export an Image with the Longvol format. A DGtal::IOException
+     * is thrown in case of io problems.
+     * 
+     * @param filename name of the output file
+     * @param aImage the image to export
+     * 
+     * @return true if no errors occur.
+     */
+    static bool exportLongvol(const std::string & filename, const Image &aImage) 
+      throw(DGtal::IOException);
+    
+
+  private: 
+    
+    /** 
+     * Generic write word (binary mode) in little-endian.
+     * 
+     * @param outs output stream.
+     * @param value value to write.
+     * 
+     * @return modified stream.
+     */
+    template <typename Word>
+    static
+    ostream& write_word( ostream& outs, Word value )
+    {
+      for (unsigned size = sizeof( Word ); size; --size, value >>= 8)
+	outs.put( static_cast <char> (value & 0xFF) );
+      return outs;
+    }
+    
+  };
+}//namespace
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/io/writers/LongvolImageWriter.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined LongvolWriter_h
+
+#undef LongvolImageWriter_RECURSES
+#endif // else defined(LongvolWriter_RECURSES)
